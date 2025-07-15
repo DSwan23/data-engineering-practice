a557bc373dc033d3c1039ec89aef0aad45a8bed4
Douglas Swanson <douglas.swanson23@gmail.com>
2025-07-14 22:27:39 -0600

    Create the stub for the test case file

---
+++ Exercises/Exercise-1/test.py
@@ -0,0 +1,11 @@
+import unittest
+import main
+
+class TestMain(unittest.TestCase):
+
+    def test_create_dir(self):
+        # TODO: Write Test Case for create_dir function
+        self.assertTrue()
+
+if __name__ == '__main__':
+    unittest.main()
