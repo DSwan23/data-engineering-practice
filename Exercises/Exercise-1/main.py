39a397f834cb03f304b78d6cfcaea2f4920f02a4
Douglas Swanson <douglas.swanson23@gmail.com>
2025-07-14 22:21:28 -0600

    Started to frame the code out

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -10,8 +10,29 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
 
+def create_dir(dirName: str):
+    # TODO: Add code in here to create a new directory
+    pass
+
+def download_from_uri(uri: str):
+    # TODO: Download a file from a URI, include error checking
+    pass
+
+def split_filename(uri: str) -> str:
+    # TODO: return the filename from the passed URI
+    pass
+
+def extract_csv_from_zip(zipFile: str) -> str:
+    # TODO: extract the csv from the zip file\
+    # TODO: remove the zip file
+    # TODO: return the csv filename
+    pass
 
 def main():
+    # TODO: write unit tests for the files above (Learn how to do this!)
+    # TODO: get the basic code working
+    # TODO: try todo it in an async manner
+    # TODO: try it in a multithreaded manner
     # your code here
     pass
 
